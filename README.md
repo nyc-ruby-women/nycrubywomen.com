1be228a598e9e1f6109cda606d46fe8e160b3f2b
Rachel Ober <rachelober@gmail.com>
2012-02-25 17:23:32 -0500

    Initializing the NYC Ruby Women Homepage project.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Homepage for NYC Ruby Women
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NycrubywomenCom::Application.config.secret_token = '48eb08debeeccdff239cfa824a1820c824fed6338d47a528b3bd522430ba1730f6fb4710380d3469796f352d25d950bd2ae102f565e64d65bf0b72cc7e097990'
